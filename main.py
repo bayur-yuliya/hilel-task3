6ed1c190a47e624187689dae4c56f72b07b05cc5
Yuliya Bayur <juliy14497@gmail.com>
2023-09-19 00:28:35 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,95 @@
+from flask import Flask, request
+from faker import Faker
+import requests
+import csv
+
+app = Flask(__name__)
+fake = Faker()
+
+
+@app.route('/requrements')
+def index():
+    try:
+
+        requirements_packages = ''
+
+        with open('requirements.txt', 'r') as file:
+            requirements = file.readlines()
+
+            for package in requirements:
+                requirements_packages += package + "<br>"
+
+        return requirements_packages
+
+    except FileNotFoundError:
+        return "Немає такого файлу"
+
+
+@app.route('/users/generate')
+def faker_func():
+
+    query_param = request.args.get('query')
+
+    if query_param is not None:
+
+        try:
+            count_users = int(request.args.get('query'))
+        except ValueError:
+            return "<h2>Помилка, введіть число</h2>"
+
+    else:
+        count_users = 100
+
+    fake_data = ""
+
+    for i in range(count_users):
+        fake_data += str(i + 1) + ".   " + fake.name() + ' - ' + fake.email() + "<br>"
+
+    return fake_data
+
+
+@app.route('/mean/')
+def mean():
+    try:
+        height_cm_sum = 0
+        weight_kg_sum = 0
+        i = 0
+
+        with open('hw.csv', 'r') as file:
+            reader = csv.reader(file)
+
+            for line in reader:
+                if len(line) > 0:
+
+                    if line[0] == "Index":
+                        continue
+                    else:
+                        height_cm_sum += float(line[1])
+                        weight_kg_sum += float(line[2])
+                        i += 1
+
+        average_height_cm = round(float(height_cm_sum/i) * 2.54, 2)
+        average_weight_kg = round(float(weight_kg_sum/i) / 2.205, 2)
+
+        return f"Середній зріст: {average_height_cm}, середня вага: {average_weight_kg}"
+
+    except FileNotFoundError:
+        return f"Не найден файл"
+
+
+@app.route('/space/')
+def space():
+    query = requests.get('http://api.open-notify.org/astros.json')
+
+    if query.json()['number']:
+        a = query.json()['number']
+    elif query.json()['people']:
+        a = len(query.json()['people'])
+    else:
+        return " Невказвна кількість космонавтів на орбіті"
+
+    return f' Kількість космонавтів на орбіті: {a}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
